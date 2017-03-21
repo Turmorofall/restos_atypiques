9a9d86355446f46b9bb51f23e31fa4b963ce7c91
florian leroux <f.j.j.leroux@gmail.com>
2017-03-21 13:17:00 +0100

    syntax gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 
-
 var minify = require('gulp-minify');
 var babel = require('gulp-babel');
 
@@ -8,7 +7,7 @@ var minifyCss = require('gulp-minify-css'); // appel le module gulp-minify-css
 
 gulp.task('css', function () {
 	gulp.src('*.css') // prend tous les fichiers CSS dans le répertoire src/css
-		.pipe(minifyCss()) //compresser les css
+		.pipe(minifyCss()) // compresser les css
 		.pipe(gulp.dest('dist/css/')); // gulp.dest permet de placer dans un dossier distant
 });
 
